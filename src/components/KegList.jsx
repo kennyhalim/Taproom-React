a2d5ee86a4c59298ebe318fd7501201688a65fd2
Kenny Halim <kennyhalim98@gmail.com>
2019-04-26 09:20:41 -0700

    add router link to keglist

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function KegList() {
   return (
     <div>
-      <h1>Keg List</h1>
+      <Link to="/">
+        <h1>Keg List</h1>
+      </Link>
     </div>
   );
 }
